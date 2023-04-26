6919f4a195824503723683f1cd0893b984507011
kalema ronald <kalemar8@gmail.com>
2023-04-26 06:56:56 -0700

    write handlers

--- write_handlers.c
+++ write_handlers.c
@@ -108,12 +108,10 @@ int write_num(int ind, char buffer[], int flags, int width, int prec,
 		buffer[ind] = padd = ' ';
 	if (prec > 0 && prec < length)
 		padd = ' ';
-
 	while (prec > length)
 		buffer[--ind] = '0', length++;
 	if (extra_c != 0)
 		length++;
-
 	if (width > length)
 	{
 		for (i = 1; i < width - length + 1; i++)
@@ -217,16 +215,13 @@ int write_pointer(char buffer[], int ind, int length, int width,
 		for (i = 3; i < width - length + 3; i++)
 			buffer[i] = padd;
 		buffer[i] = '\0';
-
 		if (flags & F_MINUS && padd == ' ')
 		{
 			buffer[--ind] = 'x';
 			buffer[--ind] = '0';
-
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[ind], length) +
-					write(1, &buffer[3], i - 3));
+			return (write(1, &buffer[ind], length) + write(1, &buffer[3], i - 3));
 		}
 		else if (!(flags & F_MINUS) && padd == ' ')
 		{
@@ -238,7 +233,6 @@ int write_pointer(char buffer[], int ind, int length, int width,
 			return (write(1, &buffer[3], i - 3) +
 					write(1, &buffer[ind], length));
 		}
-
 		else if (!(flags & F_MINUS) && padd == '0')
 		{
 			if (extra_c)
